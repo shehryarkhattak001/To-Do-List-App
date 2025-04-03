635f285a0d75e1fe619a65f6b0888ec4450f60ed
Shehryar Khan <shehryarkhattak001>
2025-04-03 18:23:33 +0500

    Adding login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,100 @@
+// import React, { useState, useEffect } from "react";
+// import Form from "./components/Form";
+// import TodoList from "./components/TodoList";
+// import TodoFilter from "./components/TodoFilter";
+// // import Login from "./components/Login";
+// import Signup from "./components/Signup/Signup";
+// import Login from "./components/Login/Login";
+
+// const App = () => {
+//   const [todos, setTodos] = useState([]);
+//   const [filter, setFilter] = useState("all");
+//   const [isEditTodo, setIsEditTodo] = useState(null);
+
+//   useEffect(() => {
+//     async function fetchTodos() {
+//       try {
+//         const response = await fetch("http://localhost:5000/todos");
+//         const data = await response.json();
+//         setTodos(data);
+//       } catch (error) {
+//         console.error("error fetching todos", error);
+//       }
+//     }
+//     fetchTodos();
+//   }, []);
+
+//   function addTodo(todo) {
+//     setTodos([...todos, todo]);
+//   }
+
+//   function deleteTodo(id) {
+//     setTodos(todos.filter((todo) => todo.id !== id));
+//   }
+
+//   function toggleCheckBox(id) {
+//     setTodos(
+//       todos.map((todo) =>
+//         todo.id === id ? { ...todo, completed: !todo.completed } : todo
+//       )
+//     );
+//   }
+
+//   const filteredTodos = todos.filter((todo) => {
+//     if (filter === "completed") {
+//       return todo.completed;
+//     }
+//     if (filter === "pending") {
+//       return !todo.completed;
+//     }
+//     return true;
+//   });
+
+//   return (
+//     <header className="main-header">
+//       <Signup />
+//       <Login />
+//       <div className="card">
+//         <h2 className="title">To-Do List App</h2>
+//         <Form
+//           addTodo={addTodo}
+//           isEditTodo={isEditTodo}
+//           updatedTodos={(updatedTodo) => {
+//             setTodos(
+//               todos.map((todo) =>
+//                 todo.id === updatedTodo.id ? updatedTodo : todo
+//               )
+//             );
+//             setIsEditTodo(null);
+//           }}
+//         />
+//         <TodoFilter setFilter={setFilter} filter={filter} />
+
+//         <TodoList
+//           todos={filteredTodos}
+//           deleteTodo={deleteTodo}
+//           toggleCheckBox={toggleCheckBox}
+//           setIsEditTodo={setIsEditTodo}
+//         />
+//       </div>
+//     </header>
+//   );
+// };
+
+// export default App;
+
 import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Form from "./components/Form";
 import TodoList from "./components/TodoList";
 import TodoFilter from "./components/TodoFilter";
+import Signup from "./components/Signup/Signup";
+import Login from "./components/Login/Login";
 
 const App = () => {
   const [todos, setTodos] = useState([]);
@@ -48,31 +141,43 @@ const App = () => {
   });
 
   return (
-    <header className="main-header">
-      <div className="card">
-        <h2 className="title">To-Do List App</h2>
-        <Form
-          addTodo={addTodo}
-          isEditTodo={isEditTodo}
-          updatedTodos={(updatedTodo) => {
-            setTodos(
-              todos.map((todo) =>
-                todo.id === updatedTodo.id ? updatedTodo : todo
-              )
-            );
-            setIsEditTodo(null);
-          }}
-        />
-        <TodoFilter setFilter={setFilter} filter={filter} />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route
+          path="/todo"
+          element={
+            <header className="main-header">
+              <div className="card">
+                <h2 className="title">To-Do List App</h2>
+                <Form
+                  addTodo={addTodo}
+                  isEditTodo={isEditTodo}
+                  updatedTodos={(updatedTodo) => {
+                    setTodos(
+                      todos.map((todo) =>
+                        todo.id === updatedTodo.id ? updatedTodo : todo
+                      )
+                    );
+                    setIsEditTodo(null);
+                  }}
+                />
+                <TodoFilter setFilter={setFilter} filter={filter} />
 
-        <TodoList
-          todos={filteredTodos}
-          deleteTodo={deleteTodo}
-          toggleCheckBox={toggleCheckBox}
-          setIsEditTodo={setIsEditTodo}
+                <TodoList
+                  todos={filteredTodos}
+                  deleteTodo={deleteTodo}
+                  toggleCheckBox={toggleCheckBox}
+                  setIsEditTodo={setIsEditTodo}
+                />
+              </div>
+            </header>
+          }
         />
-      </div>
-    </header>
+      </Routes>
+    </Router>
   );
 };
 
---
+++ frontend/src/components/Login/Login.jsx
@@ -0,0 +1,132 @@
+import React, { useState } from "react";
+import "./login.css";
+
+const Login = ({ toggleForm, setToken }) => {
+  const [loginForm, setLoginForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [error, setError] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [backendError, setBackendError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [successMessage, setSuccessMessage] = useState("");
+
+  const loginFormHandleChange = (key, value) => {
+    setLoginForm({
+      ...loginForm,
+      [key]: value,
+    });
+  };
+
+  const submitHandle = async (e) => {
+    e.preventDefault();
+
+    let newError = {
+      email: "",
+      password: "",
+    };
+
+    if (!loginForm.email.trim()) {
+      newError.email = "Email is required";
+    }
+
+    if (!loginForm.password.trim()) {
+      newError.password = "Password is required";
+    }
+
+    if (newError.email || newError.password) {
+      setError(newError);
+      return;
+    }
+
+    setSuccessMessage("");
+    setError({
+      email: "",
+      password: "",
+    });
+
+    setIsLoading(true);
+
+    try {
+      const response = await fetch("http://localhost:3000/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(loginForm),
+      });
+      const data = await response.json();
+
+      if (!response.ok) {
+        if (data.message) {
+          setBackendError(data.message);
+        }
+        return;
+      }
+      if (response.ok) {
+        setSuccessMessage("Login successful! Redirecting...");
+        setLoginForm({
+          email: "",
+          password: "",
+        });
+
+        setTimeout(() => {
+          setToken(data.token);
+          localStorage.setItem("token", data.token);
+        }, 500);
+      }
+    } catch (error) {
+      setError({
+        email: "Something went wrong. Please try again later.",
+        password: "",
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <form className="form" onSubmit={submitHandle}>
+      <h2>Login</h2>
+
+      {successMessage && <p className="success">{successMessage}</p>}
+      {backendError && <p className="error">{backendError}</p>}
+
+      <input
+        type="email"
+        className="input"
+        placeholder="Email"
+        value={loginForm.email}
+        onChange={(e) => loginFormHandleChange("email", e.target.value)}
+      />
+      {error.email && <p className="error">*{error.email}</p>}
+
+      <input
+        type="password"
+        className="input"
+        placeholder="Password"
+        value={loginForm.password}
+        onChange={(e) => loginFormHandleChange("password", e.target.value)}
+      />
+      {error.password && <p className="error">*{error.password}</p>}
+
+      <button className="btn" type="submit" disabled={isLoading}>
+        {isLoading ? "Logging In..." : "Log In"}
+      </button>
+
+      <p>
+        Don't have an account?
+        <span className="toggle-form" onClick={toggleForm}>
+          Sign Up
+        </span>
+      </p>
+    </form>
+  );
+};
+
+export default Login;
